d5a5dd2c4c47e80b08f22cac9ae8bc533488232b
root <root@ip-172-31-84-207.ec2.internal>
2023-02-16 00:26:27 +0000

    added two new subnets

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "vorx-subnet-pub-1a" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Public-Subnet-1a"
+    Name = "prod-public-subnet-1a"
   }
 }
 
@@ -14,10 +14,29 @@ resource "aws_subnet" "vorx-subnet-priv-1a" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Private-Subnet-1a"
+    Name = "prod-private-subnet-1a"
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1b" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "prod-public-subnet-1b"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1b" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.20.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "prod-private-subnet-1b"
+  }
+}
 
 output "vorx_prod_subnet_pub-1a" {
 value = aws_subnet.vorx-subnet-pub-1a.id
